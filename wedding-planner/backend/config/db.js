d2a46edf19d48d7213c6fabcb1e95d63081ed3b2
Arpit Karna <arpitkarna8a@gmail.com>
2026-07-10 20:00:10 +0530

    Update db.js

--- wedding-planner/backend/config/db.js
+++ wedding-planner/backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   console.log("NODE_ENV:", process.env.NODE_ENV);
-  console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+  console.log("MONGODB_URI exists:", !!process.env.MONGODB_URI);
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("Connected:", conn.connection.host);
   } catch (err) {
